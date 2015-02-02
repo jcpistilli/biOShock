554797951dabb2cc5c7930482098140d88fb0e11
Jonathan Pistilli <jonathan.pistilli1@marist.edu>
2015-02-02 13:59:57 -0500

    readyQueue
    Forgot I had made it a queue and not an array. Changed it to .enqueue.
    However, still not able to kill a running process

--- source/scripts/os/shell.ts
+++ source/scripts/os/shell.ts
@@ -444,7 +444,7 @@ module biOShock {
                 if (thisProgram.state !== "Terminated.")
                 {
                     thisProgram.state = "Ready.";
-                    _ReadyQueue.push(thisProgram);
+                    _ReadyQueue.enqueue(thisProgram);
                     _KernelInterruptQueue.enqueue(new Interrupt(EXECUTING_IRQ, args[0]));
                 }
                 else
